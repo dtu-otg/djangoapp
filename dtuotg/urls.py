475c7b76efd06e9acf6be8e03af09ba48cc6eef5
Anurag Hooda <specdrake@gmail.com>
2021-06-21 17:20:39 +0530

    https schema generation using drf_yasg

--- dtuotg/urls.py
+++ dtuotg/urls.py
@@ -21,6 +21,13 @@ from rest_framework import permissions
 from django.conf import settings
 from django.conf.urls.static import static
 from timetable.views import TimeTableView
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -32,6 +39,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="Test License"),
     ),
     public=True,
+    generator_class=BothHttpAndHttpsSchemaGenerator, 
     permission_classes=(permissions.AllowAny,),
 )
 
