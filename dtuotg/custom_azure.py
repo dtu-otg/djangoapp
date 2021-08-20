47eac1e33718c6bf58c5c08b1b19b78c099b0bc8
Anurag Hooda <specdrake@gmail.com>
2021-08-20 17:50:45 +0530

    change azure storage to dtuotgbeta

--- dtuotg/custom_azure.py
+++ dtuotg/custom_azure.py
@@ -1,13 +1,13 @@
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'dtuotgstorage'
-    account_key = 'QLcr6O+Q8Pnr7QFv+eXUr0SUdck5GW86FVg4E8dBjXVwbwHCUvoAN9wz2GeC5ojkbjuQLp8zF5FD07LGooWNAg==' # 
+    account_name = 'dtuotgbeta'
+    account_key = '/b8WiGENJKaq1EZXEtpKWNcDombSV4ABubK80IbjftchY16bWq94Q9FvSqK69o+C5EoG0/gdwZk5DhVXa2kITQ=='
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'dtuotgstorage'
-    account_key = 'QLcr6O+Q8Pnr7QFv+eXUr0SUdck5GW86FVg4E8dBjXVwbwHCUvoAN9wz2GeC5ojkbjuQLp8zF5FD07LGooWNAg==' # 
+    account_name = 'dtuotgbeta'
+    account_key = '/b8WiGENJKaq1EZXEtpKWNcDombSV4ABubK80IbjftchY16bWq94Q9FvSqK69o+C5EoG0/gdwZk5DhVXa2kITQ=='
     azure_container = 'static'
     expiration_secs = None
